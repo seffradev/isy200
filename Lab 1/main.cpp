c91171c6bcf1a220348bfdce6d8876746cd4c05d
jvjulia <julia.voss@student.hv.se>
2024-04-17 15:43:10 +0200

    Hela skiten

--- Lab 1/main.cpp
+++ Lab 1/main.cpp
@@ -1,6 +1,55 @@
-#include <iostream>
+#include "m.bed.h"
+
+DigitalOut red(p18)
+DigitalOut yellow(p20)
+DigitalOut green(p21)
+DigitalIn button(p23)
+
+enum Mode{
+    GoToGreen,
+    GoToRed,
+};
+
+Mode mode = GoToGreen;
+bool done = false;
+float duration = 2;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    red = 1;
+    yellow = 0;
+    green = 0;
+
+while (1) {
+    if (button == 1) { //Ändrar tillstånd tillsvidare.
+        done = false;
+        if (mode == GoToGreen) {
+            mode = GoToRed;
+        } else {
+            mode = GoToGreen;
+        }
+    }
+    if (done == false) { // Kollar tillstånd, köra sekvens eller ej samt vilken. 
+        if (mode == GoToGreen){
+            red = 1;
+            yellow = 1;
+            green = 0;
+            wait(duration);
+            red = 0;
+            yellow = 0;
+            green = 1;
+        } else {
+            red = 0;
+            yellow = 1;
+            green = 0;
+            wait (duration);
+            red = 1;
+            yellow = 0;
+            green = 0;
+        }
+        done = true;
+    }
+
 }
+}
+
+
