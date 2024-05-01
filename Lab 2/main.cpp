3a554b1590557fc18d0b0ea1ca0af872768b9f96
Hampus Avekvist <hampus.avekvist@hey.com>
2024-05-01 20:31:56 +0200

    feat: add lab 2

---
+++ Lab 2/main.cpp
@@ -0,0 +1,92 @@
+#include "mbed.h"
+#include <chrono>
+
+#define SERVO_MIN 900
+#define SERVO_MAX 2100
+#define SERVO_PERIOD 20
+#define SERVO_SCALE 1
+#define LED_PERIOD 0.001
+#define LED_SCALE 0.1
+
+// Define pins for peripherals
+PwmOut servo(p21);
+
+PwmOut redLED(p23);   // Red RGB LED
+PwmOut greenLED(p24); // Green RGB LED
+PwmOut blueLED(p25);  // Blue RGB LED
+
+AnalogIn potentiometer(p20);
+DigitalIn joystickUp(p15);
+DigitalIn joystickDown(p12);
+
+enum Direction { Left, Right };
+
+Direction direction = Direction::Right;
+int counter = 0;
+bool wiperActive = false;
+int sweepTime = 1000;
+
+void setColour(int r, int g, int b) {
+  redLED = (1 - (float)r / 255) * LED_SCALE;
+  greenLED = (1 - (float)g / 255) * LED_SCALE;
+  blueLED = (1 - (float)b / 255) * LED_SCALE;
+}
+
+void setup() {
+  // Standard servo period
+  servo.period_ms(SERVO_PERIOD);
+
+  // Servo starts at the far right position
+  servo.pulsewidth_us(SERVO_MAX);
+
+  // High frequency to control brightness via duty cycle
+  redLED.period(LED_PERIOD);
+  greenLED.period(LED_PERIOD);
+  blueLED.period(LED_PERIOD);
+
+  // Start with LED off
+  setColour(0, 0, 0);
+}
+
+int main() {
+  setup();
+
+  while (true) {
+    // Convert range [0.0, 1.0] to [1000, 5000] milliseconds
+    sweepTime = 1000 + potentiometer * 4000;
+
+    if (wiperActive) {
+      switch (direction) {
+      case Direction::Left:
+        if (counter == SERVO_SCALE * sweepTime) {
+          setColour(255, 255, 255);
+          direction = Direction::Right;
+        }
+        break;
+      case Direction::Right:
+        if (counter == SERVO_SCALE * sweepTime) {
+          setColour(0, 0, 0);
+          direction = Direction::Left;
+        }
+        break;
+      }
+    } else {
+      setColour(0, 0, 0);
+      direction = Direction::Right;
+    }
+
+    if (joystickUp) {
+      wiperActive = true;
+    }
+    if (joystickDown) {
+      wiperActive = false;
+    }
+
+    counter += direction == Direction::Left ? -1 : 1;
+
+    int stepSize = (SERVO_MAX - SERVO_MIN) / (SERVO_SCALE * sweepTime);
+    servo.pulsewidth_us(SERVO_MIN + stepSize * counter);
+
+    ThisThread::sleep_for(1);
+  }
+}
