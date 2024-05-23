8a1422d236a47ff35bcddd0f43bcc2c659fdda3f
Hampus Avekvist <hampus.avekvist@hey.com>
2024-05-23 17:08:05 +0200

    feat: add stuff to mbed to make me happy

--- mbed.h
+++ mbed.h
@@ -1,131 +1,193 @@
-#include <chrono>
+#ifndef MBED_H
+#define MBED_H
 
-const int p0 = 0;
-const int p1 = 1;
-const int p2 = 2;
-const int p3 = 3;
-const int p4 = 4;
-const int p5 = 5;
-const int p6 = 6;
-const int p7 = 7;
-const int p8 = 8;
-const int p9 = 9;
-const int p10 = 10;
-const int p11 = 11;
-const int p12 = 12;
-const int p13 = 13;
-const int p14 = 14;
-const int p15 = 15;
-const int p16 = 16;
-const int p17 = 17;
-const int p18 = 18;
-const int p19 = 19;
-const int p20 = 20;
-const int p21 = 21;
-const int p22 = 22;
-const int p23 = 23;
-const int p24 = 24;
-const int p25 = 25;
-const int p26 = 26;
-const int p27 = 27;
-const int p28 = 28;
-const int p29 = 29;
-const int p30 = 30;
+#include <chrono>
+#include <cstdio>
+#include <string>
+using namespace std::chrono_literals;
+
+typedef int PinName;
+
+const PinName p0 = 0;
+const PinName p1 = 1;
+const PinName p2 = 2;
+const PinName p3 = 3;
+const PinName p4 = 4;
+const PinName p5 = 5;
+const PinName p6 = 6;
+const PinName p7 = 7;
+const PinName p8 = 8;
+const PinName p9 = 9;
+const PinName p10 = 10;
+const PinName p11 = 11;
+const PinName p12 = 12;
+const PinName p13 = 13;
+const PinName p14 = 14;
+const PinName p15 = 15;
+const PinName p16 = 16;
+const PinName p17 = 17;
+const PinName p18 = 18;
+const PinName p19 = 19;
+const PinName p20 = 20;
+const PinName p21 = 21;
+const PinName p22 = 22;
+const PinName p23 = 23;
+const PinName p24 = 24;
+const PinName p25 = 25;
+const PinName p26 = 26;
+const PinName p27 = 27;
+const PinName p28 = 28;
+const PinName p29 = 29;
+const PinName p30 = 30;
+const PinName USBTX = 31;
+const PinName USBRX = 32;
 
 class ThisThread {
 public:
-  static void sleep_for(int ms);
-  static void sleep_for(float s);
-  static void sleep_for(double s);
-  static void sleep_for(std::chrono::milliseconds ms);
-  static void sleep_for(std::chrono::seconds s);
-  static void sleep_for(std::chrono::duration<int> ms);
-  static void sleep_for(std::chrono::duration<float> s);
-  static void sleep_for(std::chrono::duration<double> s);
+  static void sleep_for(int);
+  static void sleep_for(float);
+  static void sleep_for(double);
+  static void sleep_for(std::chrono::milliseconds);
+  static void sleep_for(std::chrono::seconds);
+  static void sleep_for(std::chrono::duration<int>);
+  static void sleep_for(std::chrono::duration<float>);
+  static void sleep_for(std::chrono::duration<double>);
 };
 
 class PwmOut {
 public:
-  PwmOut(int pin);
-  void period_ms(int period);
-  void period(float period);
-  void pulsewidth_us(int pulsewidth);
-  void pulsewidth(float pulsewidth);
-  void write(float value);
+  PwmOut(int);
+  void period_ms(int);
+  void period(float);
+  void pulsewidth_us(int);
+  void pulsewidth(float);
+  void write(float);
 
-  float operator=(float value);
-  float operator=(int value);
-  float operator=(double value);
+  float operator=(float);
+  float operator=(int);
+  float operator=(double);
 
-  friend float operator*(float lhs, PwmOut &rhs);
-  friend float operator*(PwmOut &lhs, float rhs);
-  friend float operator*(int lhs, PwmOut &rhs);
-  friend float operator*(PwmOut &lhs, int rhs);
-  friend float operator*(double lhs, PwmOut &rhs);
-  friend float operator*(PwmOut &lhs, double rhs);
+  friend float operator*(float lhs, PwmOut);
+  friend float operator*(PwmOut &lhs, float);
+  friend float operator*(int lhs, PwmOut);
+  friend float operator*(PwmOut &lhs, int);
+  friend float operator*(double lhs, PwmOut);
+  friend float operator*(PwmOut &lhs, double);
+
+  friend bool operator&&(PwmOut &lhs, bool);
+  friend bool operator&&(bool lhs, PwmOut);
+  friend bool operator&&(PwmOut &lhs, PwmOut);
+  friend bool operator||(PwmOut &lhs, bool);
+  friend bool operator||(bool lhs, PwmOut);
+  friend bool operator||(PwmOut &lhs, PwmOut);
+  friend bool operator==(PwmOut &lhs, bool);
+  friend bool operator==(bool lhs, PwmOut);
+  friend bool operator==(PwmOut &lhs, PwmOut);
+  friend bool operator!=(PwmOut &lhs, bool);
+  friend bool operator!=(bool lhs, PwmOut);
+  friend bool operator!=(PwmOut &lhs, PwmOut);
+  friend bool operator!(PwmOut);
+  operator bool();
 };
 
 class AnalogIn {
 public:
-  AnalogIn(int pin);
+  AnalogIn(int);
   float read();
 
-  friend float operator*(float lhs, AnalogIn &rhs);
-  friend float operator*(AnalogIn &lhs, float rhs);
-  friend float operator*(int lhs, AnalogIn &rhs);
-  friend float operator*(AnalogIn &lhs, int rhs);
-  friend float operator*(double lhs, AnalogIn &rhs);
-  friend float operator*(AnalogIn &lhs, double rhs);
+  friend float operator*(float lhs, AnalogIn);
+  friend float operator*(AnalogIn &lhs, float);
+  friend float operator*(int lhs, AnalogIn);
+  friend float operator*(AnalogIn &lhs, int);
+  friend float operator*(double lhs, AnalogIn);
+  friend float operator*(AnalogIn &lhs, double);
 };
 
 class DigitalIn {
 public:
-  DigitalIn(int pin);
+  DigitalIn(int);
   bool read();
 
-  const friend bool operator&&(DigitalIn &lhs, bool rhs);
-  friend bool operator&&(bool lhs, DigitalIn &rhs);
-  friend bool operator&&(DigitalIn &lhs, DigitalIn &rhs);
-  friend bool operator||(DigitalIn &lhs, bool rhs);
-  friend bool operator||(bool lhs, DigitalIn &rhs);
-  friend bool operator||(DigitalIn &lhs, DigitalIn &rhs);
+  const friend bool operator&&(DigitalIn &lhs, bool);
+  friend bool operator&&(bool lhs, DigitalIn);
+  friend bool operator&&(DigitalIn &lhs, DigitalIn);
+  friend bool operator||(DigitalIn &lhs, bool);
+  friend bool operator||(bool lhs, DigitalIn);
+  friend bool operator||(DigitalIn &lhs, DigitalIn);
 
-  friend bool operator==(DigitalIn &lhs, bool rhs);
-  friend bool operator==(bool lhs, DigitalIn &rhs);
-  friend bool operator==(DigitalIn &lhs, DigitalIn &rhs);
-  friend bool operator!=(DigitalIn &lhs, bool rhs);
-  friend bool operator!=(bool lhs, DigitalIn &rhs);
-  friend bool operator!=(DigitalIn &lhs, DigitalIn &rhs);
+  friend bool operator==(DigitalIn &lhs, bool);
+  friend bool operator==(bool lhs, DigitalIn);
+  friend bool operator==(DigitalIn &lhs, DigitalIn);
+  friend bool operator!=(DigitalIn &lhs, bool);
+  friend bool operator!=(bool lhs, DigitalIn);
+  friend bool operator!=(DigitalIn &lhs, DigitalIn);
 
-  friend bool operator!(DigitalIn &rhs);
+  friend bool operator!(DigitalIn);
 
   operator bool();
 };
 
 class DigitalOut {
 public:
-  DigitalOut(int pin);
-  void write(bool value);
+  DigitalOut(int);
+  void write(bool);
 
-  bool operator=(bool value);
-  bool operator=(int value);
+  bool operator=(bool);
+  bool operator=(int);
 
-  friend bool operator&&(DigitalOut &lhs, bool rhs);
-  friend bool operator&&(bool lhs, DigitalOut &rhs);
-  friend bool operator&&(DigitalOut &lhs, DigitalOut &rhs);
-  friend bool operator||(DigitalOut &lhs, bool rhs);
-  friend bool operator!(DigitalOut &lhs);
+  friend bool operator&&(DigitalOut &lhs, bool);
+  friend bool operator&&(bool lhs, DigitalOut);
+  friend bool operator&&(DigitalOut &lhs, DigitalOut);
+  friend bool operator||(DigitalOut &lhs, bool);
+  friend bool operator!(DigitalOut);
 };
 
 class BufferedSerial {
 public:
   BufferedSerial(int, int, int);
   void set_format(int, int, int);
-  void write(char* buffer, int size);
-  char* read(char* buffer, int size);
+  void write(char *buffer, int);
+  void write(std::string, int);
+  char *read(char *buffer, int);
 
   bool writable();
   bool readable();
 
   static const int None = 0;
 };
+
+class InterruptIn {
+public:
+  InterruptIn(int);
+  void rise(void (*function)(void));
+  void fall(void (*function)(void));
+  void mode(int);
+  void enable_irq();
+  void disable_irq();
+};
+
+class Ticker {
+public:
+  Ticker();
+  void attach(void (*function)(void), std::chrono::milliseconds);
+  void detach();
+};
+
+class Stream {
+public:
+  void printf(const char *, ...);
+  void write(char *, int);
+};
+
+class SPI {};
+
+class Timer {
+public:
+  Timer();
+  void start();
+  void stop();
+  void reset();
+  std::chrono::microseconds elapsed_time();
+};
+
+#endif
